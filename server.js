a51524f7637513a6302551558ad2b7cab64debc1
nadoyy <galang29hot@gmail.com>
2025-12-26 10:52:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -22,10 +22,6 @@ const server = http.createServer(app);
 const { initSocket } = require("./utils/socket");
 initSocket(server);
 
-app.get("/api/dashboard", auth, (req, res) => {
-  res.json(req.user);
-});
-
 // Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/emergency", require("./routes/emergency"));
@@ -36,5 +32,6 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
 );
+
 
 
