0e596b20acd748f625713410c8110956edd4f947
nadoyy <galang29hot@gmail.com>
2025-12-26 11:03:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const corsOptions = {
-  origin: "http://tniamerika.vercel.app",
+  origin: "https://tniamerika.vercel.app",
   credentials: true,
 };
 
@@ -32,6 +32,7 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
 );
+
 
 
 
